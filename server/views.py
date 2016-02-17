daed2feef9d96a485d2f83c221e9d6d5403948b1
Manuel G <manuel@manuel.is>
2016-02-17 10:53:45 +0200

    Plays with os.environ.get('VAR')

--- server/views.py
+++ server/views.py
@@ -4,9 +4,12 @@
 from flask import Flask
 from server import app
 
+import os
+
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    greeting = os.environ.get('GREETING')
+    return 'Hello, '+greeting
 
 if __name__ == '__main__':
     print("Running server on http://localhost:5000")
